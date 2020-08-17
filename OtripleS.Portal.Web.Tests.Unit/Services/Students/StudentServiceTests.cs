0bc542cdc7567e90a3c2e220a5a30acee2ef448e
Hassan Habib <hassanhabib@live.com>
2020-08-16 18:26:14 -0700

    ShouldRegisterStudentAsync -> FAIL

---
+++ OtripleS.Portal.Web.Tests.Unit/Services/Students/StudentServiceTests.cs
@@ -0,0 +1,45 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using Moq;
+using OtripleS.Portal.Web.Brokers.API;
+using OtripleS.Portal.Web.Brokers.Logging;
+using OtripleS.Portal.Web.Models.Students;
+using OtripleS.Portal.Web.Services.Students;
+using Tynamix.ObjectFiller;
+
+namespace OtripleS.Portal.Web.Tests.Unit.Services.Students
+{
+    public partial class StudentServiceTests
+    {
+        private readonly Mock<IApiBroker> apiBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IStudentService studentService;
+
+        public StudentServiceTests()
+        {
+            this.apiBrokerMock = new Mock<IApiBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.studentService = new StudentService(
+                apiBroker: this.apiBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static Student CreateRandomStudent() =>
+            CreateStudentFiller().Create();
+
+        private static Filler<Student> CreateStudentFiller()
+        {
+            var filler = new Filler<Student>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(DateTimeOffset.UtcNow);
+
+            return filler;
+        }
+    }
+}
---
+++ OtripleS.Portal.Web/Services/Students/StudentService.cs
@@ -0,0 +1,32 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using OtripleS.Portal.Web.Brokers.API;
+using OtripleS.Portal.Web.Brokers.Logging;
+using OtripleS.Portal.Web.Models.Students;
+
+namespace OtripleS.Portal.Web.Services.Students
+{
+    public class StudentService : IStudentService
+    {
+        private readonly IApiBroker apiBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public StudentService(
+            IApiBroker apiBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.apiBroker = apiBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<Student> RegisterStudentAsync(Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
