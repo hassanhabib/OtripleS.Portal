7d7bee7e3aec35ddc09e96de90758112f7860416
Hassan Habib <hassanhabib@live.com>
2020-10-15 17:48:52 -0700

    ShouldThrowValidationExceptionOnRegisterIfStudentUserIdIsInvalidAndLogItAsync -> PASS

--- OtripleS.Portal.Web/Services/Students/StudentService.Validations.cs
+++ OtripleS.Portal.Web/Services/Students/StudentService.Validations.cs
@@ -22,9 +22,15 @@ namespace OtripleS.Portal.Web.Services.Students
                     throw new InvalidStudentException(
                         parameterName: nameof(Student.Id),
                         parameterValue: student.Id);
+
+                case { } when IsInvalid(student.UserId):
+                    throw new InvalidStudentException(
+                        parameterName: nameof(Student.UserId),
+                        parameterValue: student.UserId);
             }
         }
 
         private static bool IsInvalid(Guid id) => id == Guid.Empty;
+        private static bool IsInvalid(string text) => String.IsNullOrWhiteSpace(text);
     }
 }
