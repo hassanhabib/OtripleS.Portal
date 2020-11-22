430712f2f152d2f415172b5e0b4acb9ec0ef8190
Hassan Habib <hassanhabib@live.com>
2020-11-22 10:21:35 -0800

    ShouldThrowValidationExceptionOnAddIfStudentFirstNameIsInvalidAndLogItAsync -> PASS

--- OtripleS.Portal.Web/Services/StudentViews/StudentViewService.Validations.cs
+++ OtripleS.Portal.Web/Services/StudentViews/StudentViewService.Validations.cs
@@ -22,6 +22,11 @@ namespace OtripleS.Portal.Web.Services.StudentViews
                     throw new InvalidStudentViewException(
                         parameterName: nameof(StudentView.IdentityNumber),
                         parameterValue: studentView.IdentityNumber);
+                
+                case { } when IsInvalid(studentView.FirstName):
+                    throw new InvalidStudentViewException(
+                        parameterName: nameof(StudentView.FirstName),
+                        parameterValue: studentView.FirstName);
             }
         }
 
