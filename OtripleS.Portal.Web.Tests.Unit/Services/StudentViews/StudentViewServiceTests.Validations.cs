e0dc837a226f31b50dde4ae0447dc4bde79c2f9f
Hassan Habib <hassanhabib@live.com>
2020-11-22 10:20:03 -0800

    ShouldThrowValidationExceptionOnAddIfStudentFirstNameIsInvalidAndLogItAsync -> FAIL

--- OtripleS.Portal.Web.Tests.Unit/Services/StudentViews/StudentViewServiceTests.Validations.cs
+++ OtripleS.Portal.Web.Tests.Unit/Services/StudentViews/StudentViewServiceTests.Validations.cs
@@ -104,5 +104,55 @@ namespace OtripleS.Portal.Web.Tests.Unit.Services.StudentViews
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
             this.studentServiceMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("    ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfStudentFirstNameIsInvalidAndLogItAsync(
+            string invalidFirstName)
+        {
+            // given
+            StudentView randomStudentView = CreateRandomStudentView();
+            StudentView invalidStudentView = randomStudentView;
+            invalidStudentView.FirstName = invalidFirstName;
+
+            var invalidStudentViewException = new InvalidStudentViewException(
+                parameterName: nameof(StudentView.FirstName),
+                parameterValue: invalidStudentView.FirstName);
+
+            var expectedStudentViewValidationException =
+                new StudentViewValidationException(invalidStudentViewException);
+
+            // when
+            ValueTask<StudentView> addStudentViewTask =
+                this.studentViewService.AddStudentViewAsync(invalidStudentView);
+
+            // then
+            await Assert.ThrowsAsync<StudentViewValidationException>(() =>
+               addStudentViewTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedStudentViewValidationException))),
+                        Times.Once);
+
+            this.userServiceMock.Verify(service =>
+                service.GetCurrentlyLoggedInUser(),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Never);
+
+            this.studentServiceMock.Verify(service =>
+                service.RegisterStudentAsync(It.IsAny<Student>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.userServiceMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.studentServiceMock.VerifyNoOtherCalls();
+        }
     }
 }
