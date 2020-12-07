f39717beee5a35ece45171efc67171afcb0554a8
Hassan Habib <hassanhabib@live.com>
2020-12-06 22:43:48 -0800

    ShouldInitializeComponent -> PASS

---
+++ OtripleS.Portal.Web.Tests.Unit/Views/StudentRegistrationComponents/StudentRegistrationComponentTests.Render.cs
@@ -0,0 +1,35 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using FluentAssertions;
+using OtripleS.Portal.Web.Models.ContainerComponents;
+using OtripleS.Portal.Web.Views.Components;
+using Xunit;
+
+namespace OtripleS.Portal.Web.Tests.Unit.Views.StudentRegistrationComponents
+{
+    public partial class StudentRegistrationComponentTests
+    {
+        [Fact]
+        public void ShouldInitializeComponent()
+        {
+            // given
+            ComponentState expectedComponentState =
+                ComponentState.Loading;
+
+            // when
+            var initialStudentRegistrationComponent = new StudentRegistrationComponent();
+
+            // then
+            initialStudentRegistrationComponent.State.Should().Be(expectedComponentState);
+            initialStudentRegistrationComponent.Exception.Should().BeNull();
+            initialStudentRegistrationComponent.StudentIdentityTextBox.Should().BeNull();
+            initialStudentRegistrationComponent.StudentFirstNameTextBox.Should().BeNull();
+            initialStudentRegistrationComponent.StudentMiddleNameTextBox.Should().BeNull();
+            initialStudentRegistrationComponent.StudentLastNameTextBox.Should().BeNull();
+            initialStudentRegistrationComponent.StudentView.Should().BeNull();
+        }
+    }
+}
---
+++ OtripleS.Portal.Web.Tests.Unit/Views/StudentRegistrationComponents/StudentRegistrationComponentTests.cs
@@ -0,0 +1,26 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using Bunit;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using OtripleS.Portal.Web.Services.StudentViews;
+using OtripleS.Portal.Web.Views.Components;
+
+namespace OtripleS.Portal.Web.Tests.Unit.Views.StudentRegistrationComponents
+{
+    public partial class StudentRegistrationComponentTests : TestContext
+    {
+        private readonly Mock<IStudentViewService> studentViewServiceMock;
+        private IRenderedComponent<StudentRegistrationComponent> renderedStudentRegistrationComponent;
+
+        public StudentRegistrationComponentTests()
+        {
+            this.studentViewServiceMock = new Mock<IStudentViewService>();
+            this.Services.AddScoped(services => this.studentViewServiceMock.Object);
+            this.Services.AddServerSideBlazor();
+        }
+    }
+}
