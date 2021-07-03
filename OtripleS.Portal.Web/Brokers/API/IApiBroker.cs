126dbdd95141dbfab3e2deada5a907c14a3797e4
evannlim <evann@piorsoft.com>
2021-07-02 17:30:33 -0700

    BROKERS: IApiBroker

--- OtripleS.Portal.Web/Brokers/API/IApiBroker.cs
+++ OtripleS.Portal.Web/Brokers/API/IApiBroker.cs
@@ -3,10 +3,15 @@
 // FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
 // ---------------------------------------------------------------
 
+using System.Threading.Tasks;
+
 namespace OtripleS.Portal.Web.Brokers.API
 {
     public partial interface IApiBroker
     {
-
+        ValueTask<T> GetAsync<T>(string relativeUrl);
+        ValueTask<T> PostAsync<T>(string relativeUrl, T content);
+        ValueTask<T> PutAsync<T>(string relativeUrl, T content);
+        ValueTask<T> DeleteAsync<T>(string relativeUrl);
     }
 }
