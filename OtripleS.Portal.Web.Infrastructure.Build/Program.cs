5e391ef6559008020de5d26aa39e00c9dde96046
Shri Humrudha Jagathisun <shrija@microsoft.com>
2021-10-11 13:21:23 -0700

    INFRA: Setup Build Project

---
+++ OtripleS.Portal.Web.Infrastructure.Build/Program.cs
@@ -0,0 +1,81 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+namespace OtripleS.Portal.Web.Infrastructure.Build
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var adotNetClient = new ADotNetClient();
+
+            var githubPipeline = new GithubPipeline
+            {
+                Name = ".Net",
+
+                OnEvents = new Events
+                {
+                    Push = new PushEvent
+                    {
+                        Branches = new string[] { "master" }
+                    },
+                    PullRequest = new PullRequestEvent
+                    {
+                        Branches = new string[] { "master" }
+                    }
+                },
+
+                Jobs = new Jobs
+                {
+                    Build = new BuildJob
+                    {
+                        RunsOn = BuildMachines.Windows2019,
+
+                        Steps = new List<GithubTask>
+                        {
+                            new CheckoutTaskV2
+                            {
+                                Name = "Check Out"
+                            },
+
+                            new SetupDotNetTaskV1
+                            {
+                                Name = "Setup .Net",
+                                TargetDotNetVersion = new TargetDotNetVersion
+                                {
+                                    DotNetVersion = "6.0.100-rc.1.21463.6",
+                                    IncludePrerelease = true
+                                }
+                            },
+
+                            new RestoreTask
+                            {
+                                Name = "Restore"
+                            },
+
+                            new DotNetBuildTask
+                            {
+                                Name = "Build"
+                            },
+
+                            new TestTask
+                            {
+                                Name = "Test"
+                            }
+                        }
+                    }
+                }
+            };
+
+            adotNetClient.SerializeAndWriteToFile(githubPipeline, "../../../../.github/workflows/dotnet.yml");
+        }
+    }
+}
