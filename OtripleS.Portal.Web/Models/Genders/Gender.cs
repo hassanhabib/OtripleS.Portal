387684de36a02dfdaa066005e571462726ac9678
Milan Toncic <milantoncic@outlook.com>
2021-10-21 20:21:15 +0200

    Introduced Teacher's model

---
+++ OtripleS.Portal.Web/Models/Genders/Gender.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OtripleS.Portal.Web.Models.Genders
+{
+    public enum Gender
+    {
+    }
+}
