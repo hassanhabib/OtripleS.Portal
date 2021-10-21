c99514e5cceb0e9593769b09224757f1d4394523
Milan Toncic <milantoncic@outlook.com>
2021-10-21 20:28:08 +0200

    Introduced Teacher's Foundation Service

---
+++ OtripleS.Portal.Web/Services/Teachers/ITeacherService.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using OtripleS.Portal.Web.Models.Teachers;
+
+namespace OtripleS.Portal.Web.Services
+{
+    public interface ITeacherService
+    {
+        ValueTask<IEnumerable<Teacher>> RetrieveAllTeachersAsync();
+    }
+}
---
+++ OtripleS.Portal.Web/Services/Teachers/TeacherService.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using OtripleS.Portal.Web.Brokers.API;
+using OtripleS.Portal.Web.Brokers.Logging;
+using OtripleS.Portal.Web.Models.Teachers;
+
+namespace OtripleS.Portal.Web.Services.Teachers
+{
+    public class TeacherService : ITeacherService
+    {
+        private readonly IApiBroker apiBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public TeacherService(
+            IApiBroker apiBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.apiBroker = apiBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<IEnumerable<Teacher>> RetrieveAllTeachersAsync() =>
+            throw new System.NotImplementedException();
+    }
+}
