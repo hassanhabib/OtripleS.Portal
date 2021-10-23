fd3fcd4372206300467e6e9be67f46eae86a3fa0
Brian Parker <brian.parker@hotmail.com.au>
2021-10-24 00:35:39 +1100

    CODE RUB: Add Teachers model Add TeachStatus enum Add Gender enum

---
+++ OtripleS.Portal.Web/Brokers/API/ApiBroker.Teachers.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using OtripleS.Portal.Web.Models.Teachers;
+
+namespace OtripleS.Portal.Web.Brokers.API
+{
+    public partial class ApiBroker
+    {
+        public ValueTask<IEnumerable<Teacher>> GetAllTeachersAsync() =>
+            throw new System.NotImplementedException();
+    }
+}
---
+++ OtripleS.Portal.Web/Models/Teachers/TeacherStatus.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+namespace OtripleS.Portal.Web.Models.Teachers
+{
+    public enum TeacherStatus
+    {
+        Active,
+        Retired,
+        Terminated
+    }
+}
