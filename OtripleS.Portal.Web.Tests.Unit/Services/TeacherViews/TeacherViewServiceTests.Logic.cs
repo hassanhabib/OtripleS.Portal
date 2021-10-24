e760bda019fbf630803a7696c6d08152ad48dd81
Milan Toncic <milantoncic@outlook.com>
2021-10-25 00:11:48 +0200

    ShouldRetrieveTeachersViews-> FAIL

---
+++ OtripleS.Portal.Web.Tests.Unit/Services/TeacherViews/TeacherViewServiceTests.Logic.cs
@@ -0,0 +1,52 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using FluentAssertions;
+using Moq;
+using OtripleS.Portal.Web.Models.Teachers;
+using OtripleS.Portal.Web.Models.TeacherViews;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OtripleS.Portal.Web.Tests.Unit.Services.TeacherViews
+{
+    public partial class TeacherViewServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveTeachersViews()
+        {
+            List<Teacher> randomTeachers = CreateRandomTeachers();
+            List<Teacher> retrievedServiceTeachers = randomTeachers;
+            List<TeacherView> expectedTeachers =
+                retrievedServiceTeachers.Select(retrievedServiceTeacher =>
+                    new TeacherView
+                    {
+                        EmployeeNumber = retrievedServiceTeacher.EmployeeNumber,
+                        FirstName = retrievedServiceTeacher.FirstName,
+                        MiddleName = retrievedServiceTeacher.MiddleName,
+                        LastName = retrievedServiceTeacher.LastName,
+                        Gender = (TeacherGenderView)retrievedServiceTeacher.Gender,
+                        Status = (TeacherStatusView)retrievedServiceTeacher.Status,
+                    }).ToList();
+
+            this.teacherServiceMock.Setup(service =>
+                service.RetrieveAllTeachersAsync())
+                    .ReturnsAsync(retrievedServiceTeachers);
+
+            IReadOnlyList<TeacherView> retrievedTeachers = 
+                await this.teacherViewService.RetrieveAllTeachers();
+
+            retrievedTeachers.Should().BeEquivalentTo(expectedTeachers);
+
+            this.teacherServiceMock.Verify(service => 
+                service.RetrieveAllTeachersAsync(),
+                    Times.Once());
+
+            this.teacherServiceMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ OtripleS.Portal.Web.Tests.Unit/Services/TeacherViews/TeacherViewServiceTests.cs
@@ -0,0 +1,47 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using Moq;
+using OtripleS.Portal.Web.Models.Teachers;
+using OtripleS.Portal.Web.Services.Teachers;
+using OtripleS.Portal.Web.Services.TeacherViews;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tynamix.ObjectFiller;
+
+namespace OtripleS.Portal.Web.Tests.Unit.Services.TeacherViews
+{
+    public partial class TeacherViewServiceTests
+    {
+        private readonly Mock<ITeacherService> teacherServiceMock;
+        private readonly ITeacherViewService teacherViewService;
+
+        public TeacherViewServiceTests()
+        {
+            this.teacherServiceMock = new Mock<ITeacherService>();
+            this.teacherViewService = 
+                new TeacherViewService(teacherService: teacherServiceMock.Object);
+        }
+
+        private static int GetRandomNumber() => new IntRange(min: 2, max: 10).GetValue();
+
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private List<Teacher> CreateRandomTeachers() =>
+            CreateTeacherFiller().Create(count: GetRandomNumber()).ToList();
+
+        private static Filler<Teacher> CreateTeacherFiller()
+        {
+            var filler = new Filler<Teacher>();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(valueToUse: GetRandomDateTime());
+
+            return filler;
+        }
+    }
+}
