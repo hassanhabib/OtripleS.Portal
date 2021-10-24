c4afe038301495a762b33bbb7ac030442e9e9e6d
Milan Toncic <milantoncic@outlook.com>
2021-10-25 00:38:55 +0200

    ShouldThrowTeacherViewDependencyExceptionIfDependecyErrorOccursAndLogIt -> FAIL

---
+++ OtripleS.Portal.Web.Tests.Unit/Services/TeacherViews/TeacherViewServiceTests.Exceptions.cs
@@ -0,0 +1,46 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using Moq;
+using OtripleS.Portal.Web.Models.TeacherViews;
+using OtripleS.Portal.Web.Models.TeacherViews.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OtripleS.Portal.Web.Tests.Unit.Services.TeacherViews
+{
+    public partial class TeacherViewServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowTeacherViewDependencyExceptionIfDependecyErrorOccursAndLogIt()
+        {
+            var exception = new Exception();
+
+            var failedTeacherViewDependencyException =
+                new FailedTeacherViewDependencyException(exception);
+
+            var expectedTeacherViewDependencyException =
+                new TeacherViewDependencyException(failedTeacherViewDependencyException);
+
+            this.teacherServiceMock.Setup(teacherService =>
+                teacherService.RetrieveAllTeachersAsync())
+                    .Throws(exception);
+
+            ValueTask<List<TeacherView>> retrieveAllTeachersTask = 
+                this.teacherViewService.RetrieveAllTeachers();
+
+            await Assert.ThrowsAsync<TeacherViewDependencyException>(() => 
+                retrieveAllTeachersTask.AsTask());
+
+            this.teacherServiceMock.Verify(teacherService => 
+                teacherService.RetrieveAllTeachersAsync(),
+                    Times.Once);
+
+            this.teacherServiceMock.VerifyNoOtherCalls();
+        } 
+    }
+}
---
+++ OtripleS.Portal.Web/Models/TeacherViews/Exceptions/TeacherViewDependencyException.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using Xeptions;
+
+namespace OtripleS.Portal.Web.Models.TeacherViews.Exceptions
+{
+    public class TeacherViewDependencyException : Xeption
+    {
+        public TeacherViewDependencyException(Exception innerException)
+            : base(message: "Teacher view dependency error occurred, please contact support.", innerException)
+        { }
+    }
+}
