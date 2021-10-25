369e00b50f4254c1c38c8b1711e1bff836e9871d
Milan Toncic <milantoncic@outlook.com>
2021-10-25 08:53:51 +0200

    CODE RUB: Pushing mapping logic into appropriate partial

--- OtripleS.Portal.Web.Tests.Unit/Services/TeacherViews/TeacherViewServiceTests.cs
+++ OtripleS.Portal.Web.Tests.Unit/Services/TeacherViews/TeacherViewServiceTests.cs
@@ -39,7 +39,7 @@ namespace OtripleS.Portal.Web.Tests.Unit.Services.TeacherViews
         private static DateTimeOffset GetRandomDateTime() =>
             new DateTimeRange(earliestDate: new DateTime()).GetValue();
 
-        private List<Teacher> CreateRandomTeachers() =>
+        private static List<Teacher> CreateRandomTeachers() =>
             CreateTeacherFiller().Create(count: GetRandomNumber()).ToList();
 
         private static List<TeacherView> CreateExpectedTeachersViews(List<Teacher> retrievedServiceTeachers)
