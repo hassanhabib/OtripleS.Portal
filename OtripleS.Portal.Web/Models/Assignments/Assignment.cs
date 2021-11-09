d3b974a83377baab3efc303089fa28ae2f2a7427
Milan Toncic <milantoncic@outlook.com>
2021-11-09 18:11:08 +0100

    Introducing Assignment model

---
+++ OtripleS.Portal.Web/Models/Assignments/Assignment.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+
+namespace OtripleS.Portal.Web.Models.Assignments
+{
+    public class Assignment
+    {
+        public Guid Id { get; set; }
+        public string Label { get; set; }
+        public string Content { get; set; }
+        public AssignmentStatus Status { get; set; }
+    }
+}
