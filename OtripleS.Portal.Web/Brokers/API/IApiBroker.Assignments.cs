1537233f3ca106a4530743df3e9647a1e9a55c1f
Milan Toncic <milantoncic@outlook.com>
2021-11-09 18:12:24 +0100

    Adding Get All Assignments Broker

---
+++ OtripleS.Portal.Web/Brokers/API/IApiBroker.Assignments.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using OtripleS.Portal.Web.Models.Assignments;
+
+namespace OtripleS.Portal.Web.Brokers.API
+{
+    public partial interface IApiBroker
+    {
+        ValueTask<List<Assignment>> GetAllAssignmentsAsync();
+    }
+}
