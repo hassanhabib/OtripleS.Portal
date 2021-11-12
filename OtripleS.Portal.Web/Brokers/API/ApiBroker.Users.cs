62c9214125dca9fe27694772ce32e6748553afa6
TalatTanbir <hossaintanbir0@gmail.com>
2021-11-12 10:35:20 +0600

    BROKERS: GET All Users

---
+++ OtripleS.Portal.Web/Brokers/API/ApiBroker.Users.cs
@@ -0,0 +1,19 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using OtripleS.Portal.Web.Models.Users;
+
+namespace OtripleS.Portal.Web.Brokers.API
+{
+    public partial class ApiBroker
+    {
+        private const string UsersRelativeUrl = "api/users";
+
+        public async ValueTask<List<User>> GetAllUsersAsync() =>
+            await this.GetAsync<List<User>>(UsersRelativeUrl);
+    }
+}
---
+++ OtripleS.Portal.Web/Models/Users/User.cs
@@ -0,0 +1,37 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace OtripleS.Portal.Web.Models.Users
+{
+    public class User : IdentityUser<Guid>
+    {
+        public override Guid Id
+        {
+            get => base.Id;
+            set => base.Id = value;
+        }
+
+        public override string UserName
+        {
+            get => base.Email;
+            set => base.Email = value;
+        }
+
+        public override string PhoneNumber
+        {
+            get => base.PhoneNumber;
+            set => base.PhoneNumber = value;
+        }
+
+        public string Name { get; set; }
+        public string FamilyName { get; set; }
+        public UserStatus Status { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+    }
+}
