6ce170d8650681864eb17ae145d40c62418404c1
SallyDeveloper <fsaadat@ricres.org>
2021-12-02 13:21:55 -0800

    PEOVISION: Initialize Cloud Broker

---
+++ OtripleS.Portal.Web.Infrastructure.Provision/Brokers/Clouds/CloudBroker.cs
@@ -0,0 +1,44 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Authentication;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+
+namespace OtripleS.Portal.Web.Infrastructure.Provision.Brokers.Clouds
+{
+    public partial class CloudBroker : ICouldBroker
+    {
+        private readonly string clientId;
+        private readonly string clientSecret;
+        private readonly string tenantId;
+        private readonly IAzure azure;
+
+        public CloudBroker()
+        {
+            this.clientId = Environment.GetEnvironmentVariable("AzureClientId");
+            this.clientSecret = Environment.GetEnvironmentVariable("AzureClientSecret");
+            this.tenantId = Environment.GetEnvironmentVariable("AzureTenantId");
+            this.azure = AutneticateAzure();
+        }
+
+        private IAzure AutneticateAzure()
+        {
+            AzureCredentials credentials =
+                SdkContext.AzureCredentialsFactory.FromServicePrincipal(
+                    clientId: this.clientId,
+                    clientSecret: this.clientSecret,
+                    tenantId: this.tenantId,
+                    environment: AzureEnvironment.AzureGlobalCloud);
+
+            return Azure.Configure()
+                .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
+                .Authenticate(credentials)
+                .WithDefaultSubscription();
+        }
+    }
+}
