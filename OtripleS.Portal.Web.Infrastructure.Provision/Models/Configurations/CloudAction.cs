0ea15d670106d07be5255012797b11a932459d78
SallyDeveloper <fsaadat@ricres.org>
2021-12-02 19:33:12 -0800

    BROKERS: Implement Configuration Broker

---
+++ OtripleS.Portal.Web.Infrastructure.Provision/Models/Configurations/CloudAction.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System.Collections.Generic;
+
+namespace OtripleS.Portal.Web.Infrastructure.Provision.Models.Configurations
+{
+    public class CloudAction
+    {
+        public List<string> Environmnents { get; set; }      
+    }
+}
---
+++ OtripleS.Portal.Web.Infrastructure.Provision/Models/Configurations/CloudManagementConfiguration.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+namespace OtripleS.Portal.Web.Infrastructure.Provision.Models.Configurations
+{
+    public class CloudManagementConfiguration
+    {
+        public string ProjectName { get; set; }
+        public CloudAction Up { get; set; }
+        public CloudAction Down { get; set; }
+    }
+}
