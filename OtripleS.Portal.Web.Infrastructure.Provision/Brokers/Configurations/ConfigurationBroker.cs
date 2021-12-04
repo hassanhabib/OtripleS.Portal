003deef8809b9546611ba0fc9f804da09d024940
SallyDeveloper <fsaadat@ricres.org>
2021-12-04 15:42:57 -0800

    BROKERS: Implement Configuration Broker

---
+++ OtripleS.Portal.Web.Infrastructure.Provision/Brokers/Configurations/ConfigurationBroker.cs
@@ -0,0 +1,24 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using OtripleS.Portal.Web.Infrastructure.Provision.Models.Configurations;
+
+namespace OtripleS.Portal.Web.Infrastructure.Provision.Brokers.Configurations
+{
+	public class ConfigurationBroker : IConfigurationBroker
+	{
+		public CloudManagementConfiguration GetConfiguration()
+		{
+			IConfigurationRoot configuraitonRoot = new ConfigurationBuilder()
+				 .SetBasePath(basePath: Directory.GetCurrentDirectory())
+				 .AddJsonFile(path: "appSettings.json", optional: false)
+				 .Build();
+
+			return configuraitonRoot.Get<CloudManagementConfiguration>();
+		}
+	}
+}
---
+++ OtripleS.Portal.Web.Infrastructure.Provision/Brokers/Configurations/IConfigurationBroker.cs
@@ -0,0 +1,14 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using OtripleS.Portal.Web.Infrastructure.Provision.Models.Configurations;
+
+namespace OtripleS.Portal.Web.Infrastructure.Provision.Brokers.Configurations
+{
+	public interface IConfigurationBroker
+	{
+		CloudManagementConfiguration GetConfiguration();
+	}
+}
