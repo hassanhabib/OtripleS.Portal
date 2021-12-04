c5193ea69440637fae13ba29cb74308515778cc3
SallyDeveloper <fsaadat@ricres.org>
2021-12-04 15:19:43 -0800

    BROKERS: Implement Logging Broker

---
+++ OtripleS.Portal.Web.Infrastructure.Provision/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,12 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+namespace OtripleS.Portal.Web.Infrastructure.Provision.Brokers.Loggings
+{
+	public interface ILoggingBroker
+	{
+		void LogActivity(string message);
+	}
+}
