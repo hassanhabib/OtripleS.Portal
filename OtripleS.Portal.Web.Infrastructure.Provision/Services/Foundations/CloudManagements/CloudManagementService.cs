af5f34391ac8e8c70378da38a4f0cf873d14c2fc
SallyDeveloper <fsaadat@ricres.org>
2021-12-03 19:15:42 -0800

    SERVICE : Implement CLoud Managment Service

---
+++ OtripleS.Portal.Web.Infrastructure.Provision/Services/Foundations/CloudManagements/CloudManagementService.cs
@@ -0,0 +1,33 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using OtripleS.Portal.Web.Infrastructure.Provision.Brokers.Clouds;
+using OtripleS.Portal.Web.Infrastructure.Provision.Brokers.Loggings;
+
+namespace OtripleS.Portal.Web.Infrastructure.Provision.Services.Foundations.CloudManagements
+{
+    public class CloudManagementService : ICloudManagementService
+    {
+        private readonly ICloudBroker cloudBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public CloudManagementService(
+            ICloudBroker cloudBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.cloudBroker = new CloudBroker();
+            this.loggingBroker = new LoggingBroker();
+        }
+
+        public ValueTask<IResourceGroup> ProvisionResourceGroupAsync(
+            string projectName,
+            string environment)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ OtripleS.Portal.Web.Infrastructure.Provision/Services/Foundations/CloudManagements/ICloudManagementService.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace OtripleS.Portal.Web.Infrastructure.Provision.Services.Foundations.CloudManagements
+{
+    public interface ICloudManagementService
+    {
+        ValueTask<IResourceGroup> ProvisionResourceGroupAsync(
+            string projectName,
+            string environment);
+    }
+}
