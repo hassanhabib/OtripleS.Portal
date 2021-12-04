fd907e3cf1cb99a48895ce09c6e714c0a4838a5a
SallyDeveloper <fsaadat@ricres.org>
2021-12-04 14:56:09 -0800

    PROVISION: Initialize Cloud Broker

---
+++ OtripleS.Portal.Web.Infrastructure.Provision/Program.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+
+namespace OtripleS.Portal.Web.Infrastructure.Provision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
