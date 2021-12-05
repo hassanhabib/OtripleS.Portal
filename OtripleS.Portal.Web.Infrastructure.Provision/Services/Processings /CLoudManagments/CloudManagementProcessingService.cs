6a1e4f08028c2b0f5a6a2a3de017aea2e574a95a
SallyDeveloper <fsaadat@ricres.org>
2021-12-04 18:46:47 -0800

    PROCESSING: PROVISION

---
+++ OtripleS.Portal.Web.Infrastructure.Provision/Services/Processings /CLoudManagments/CloudManagementProcessingService.cs
@@ -0,0 +1,68 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using OtripleS.Portal.Web.Infrastructure.Provision.Brokers.Configurations;
+using OtripleS.Portal.Web.Infrastructure.Provision.Models.Configurations;
+using OtripleS.Portal.Web.Infrastructure.Provision.Services.Foundations.CloudManagements;
+
+namespace OtripleS.Portal.Web.Infrastructure.Provision.Services.Processings.CLoudManagments
+{
+	public class CloudManagementProcessingService : ICloudManagementProcessingService
+	{
+		private readonly ICloudManagementService cloudManagementService;
+		private readonly IConfigurationBroker configurationBroker;
+
+		public CloudManagementProcessingService()
+		{
+			this.cloudManagementService = new CloudManagementService();
+			this.configurationBroker = new ConfigurationBroker();
+		}
+
+		public async ValueTask ProcessAsync()
+		{
+			CloudManagementConfiguration cloudManagementConfiguration =
+				this.configurationBroker.GetConfiguration();
+
+			await ProvisionAsync(
+				projectName: cloudManagementConfiguration.ProjectName,
+				cloudAction: cloudManagementConfiguration.Up);
+		}
+
+		private async ValueTask ProvisionAsync(
+			string projectName,
+			CloudAction cloudAction)
+		{
+			List<string> environments = RetrieveEnvironments(cloudAction);
+
+			foreach (string environmentName in environments)
+			{
+				IResourceGroup resourceGroup = await this.cloudManagementService
+					.ProvisionResourceGroupAsync(
+						projectName,
+						environmentName);
+
+				IAppServicePlan appServicePlan = await this.cloudManagementService
+					.ProvisionPlanAsync(
+						projectName,
+						environmentName,
+						resourceGroup);
+
+				IWebApp webApp = await this.cloudManagementService
+					.ProvisionWebAppAsync(
+						projectName,
+						environmentName,
+						resourceGroup,
+						appServicePlan);
+			}
+		}
+
+		private static List<string> RetrieveEnvironments(CloudAction cloudAction) =>
+			cloudAction?.Environmnents ?? new List<string>();
+	}
+}
---
+++ OtripleS.Portal.Web.Infrastructure.Provision/Services/Processings /CLoudManagments/ICloudManagementProcessingService.cs
@@ -0,0 +1,14 @@
+﻿// -------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE FOR THE WORLD
+// -------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace OtripleS.Portal.Web.Infrastructure.Provision.Services.Processings.CLoudManagments
+{
+    public interface ICloudManagementProcessingService
+    {
+        ValueTask ProcessAsync();
+    }
+}
