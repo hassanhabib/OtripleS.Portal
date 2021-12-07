24074a0f07ecc0fe53514928dc2e2f0944c5d072
Ricky <rcruzfreelance@outlook.com>
2021-12-06 21:06:43 -0400

    EXPOSER: Run Process Async

--- OtripleS.Portal.Web.Infrastructure.Provision/Program.cs
+++ OtripleS.Portal.Web.Infrastructure.Provision/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System.Threading.Tasks;
+using OtripleS.Portal.Web.Infrastructure.Provision.Services.Processings.CloudManagements;
 
 namespace OtripleS.Portal.Web.Infrastructure.Provision
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ICloudManagementProcessingService cloudManagementProcessingService =
+                new CloudManagementProcessingService();
+
+            await cloudManagementProcessingService.ProcessAsync();
         }
     }
 }
