1e538dee70101d3c6d1eeba125f28c7e6ef3bb2c
Ricky <rcruzfreelance@outlook.com>
2021-12-06 20:27:11 -0400

    BROKER: Added logging broker

---
+++ OtripleS.Portal.Web.Infrastructure.Provision/Brokers/Loggings/ILoggingBroker.cs
@@ -0,0 +1,12 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+namespace OtripleS.Portal.Web.Infrastructure.Provision.Brokers.Loggings
+{
+    public interface ILoggingBroker
+    {
+        void LogActivity(string message);
+    }
+}
---
+++ OtripleS.Portal.Web.Infrastructure.Provision/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using System;
+
+namespace OtripleS.Portal.Web.Infrastructure.Provision.Brokers.Loggings
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        public void LogActivity(string message) =>
+            Console.WriteLine(message);
+    }
+}
