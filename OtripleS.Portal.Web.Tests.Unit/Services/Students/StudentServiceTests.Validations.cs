b2dc8b0fac11493b89e8a7adc9fbfb2439492bc6
Hassan Habib <hassanhabib@live.com>
2021-12-19 19:23:41 -0800

    ShouldThrowValidationExceptionOnRegisterIfCreatedByIdIsInvalidAndLogItAsync -> GONE

--- OtripleS.Portal.Web.Tests.Unit/Services/Students/StudentServiceTests.Validations.cs
+++ OtripleS.Portal.Web.Tests.Unit/Services/Students/StudentServiceTests.Validations.cs
@@ -46,44 +46,6 @@ namespace OtripleS.Portal.Web.Tests.Unit.Services.Students
         }
 
         [Fact]
-        public async Task ShouldThrowValidationExceptionOnRegisterIfCreatedByIdIsInvalidAndLogItAsync()
-        {
-            // given
-            Guid invalidId = Guid.Empty;
-            Student randomStudent = CreateRandomStudent();
-            Student invalidStudent = randomStudent;
-            invalidStudent.CreatedBy = invalidId;
-
-            var invalidStudentException =
-                new InvalidStudentException(
-                    parameterName: nameof(Student.CreatedBy),
-                    parameterValue: invalidStudent.CreatedBy);
-
-            var expectedStudentValidationException =
-                new StudentValidationException(invalidStudentException);
-
-            // when
-            ValueTask<Student> registerStudentTask =
-                this.studentService.AddStudentAsync(invalidStudent);
-
-            // then
-            await Assert.ThrowsAsync<StudentValidationException>(() =>
-                registerStudentTask.AsTask());
-
-            this.loggingBrokerMock.Verify(broker =>
-                broker.LogError(It.Is(
-                    SameExceptionAs(expectedStudentValidationException))),
-                        Times.Once);
-
-            this.apiBrokerMock.Verify(broker =>
-                broker.PostStudentAsync(It.IsAny<Student>()),
-                    Times.Never);
-
-            this.loggingBrokerMock.VerifyNoOtherCalls();
-            this.apiBrokerMock.VerifyNoOtherCalls();
-        }
-
-        [Fact]
         public async Task ShouldThrowValidationExceptionOnRegisterIfUpdatedByIdIsInvalidAndLogItAsync()
         {
             // given
