59545917ec981fd2c45dee618843f6396cba1ee0
Hassan Habib <hassanhabib@live.com>
2021-12-21 11:05:44 -0800

    BASES: Validation Summary

---
+++ OtripleS.Portal.Web/Views/Bases/ValidationSummaryBase.razor.cs
@@ -0,0 +1,26 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace OtripleS.Portal.Web.Views.Bases
+{
+    public partial class ValidationSummaryBase
+    {
+        [Parameter]
+        public IDictionary ValidationData { get; set; }
+
+        [Parameter]
+        public string Key { get; set; }
+
+        public IEnumerable<string> Errors
+        {
+            get => this.ValidationData?[Key] as IEnumerable<string>;
+            set => Errors = value;
+        }
+    }
+}
